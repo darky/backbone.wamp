1534b7fa8bea9bdc314439471d273a306ae909f5
Vladislav Botvin <Vladislav.Botvin@infowatch.com>
2015-07-23 20:01:28 +0300

    cleanup gulpfile from coffee

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,19 @@
 # *************** */
 var _ = require("underscore"),
   gulp = require("gulp"),
-  coffee = require("gulp-coffee"),
-  coffeelint = require("gulp-coffeelint"),
   exec = require("child_process").exec,
   mocha_phantomjs = require("gulp-mocha-phantomjs"),
-  qunit = require("gulp-qunit"),
-  sourcemaps = require("gulp-sourcemaps");
+  qunit = require("gulp-qunit");
 
 
 /* ******************
 #    CONCAT-TASKS
 # **************** */
-gulp.task("default", ["test", "build"]);
+gulp.task("default", ["test", "lint"]);
 gulp.task("test", [
     "test-backbone",
-    "test-backbone-extend",
     "test-own"
 ]);
-gulp.task("build", ["lint", "compile"]);
 
 
 /* *********
@@ -29,20 +24,6 @@ gulp.task("build", ["lint", "compile"]);
 gulp.task("test-backbone", function () {
   return gulp.src("backbone-test/index.html")
     .pipe(qunit());
-});
-
-gulp.task("test-backbone-extend-pre", function () {
-  return gulp.src("backbone-test/model.coffee")
-    .pipe(coffee())
-    .pipe(gulp.dest("tmp/"));
-});
-
-gulp.task("test-backbone-extend", ["test-backbone-extend-pre"], function (cb) {
-  exec("node tmp/model.js", function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
 });
 
 gulp.task("test-own-crossbar", function (cb) {
@@ -59,38 +40,8 @@ gulp.task("test-own", ["test-own-crossbar"], function (cb) {
 
 
 /* ***********
-#    BUILD
+#    LINT
 # ********* */
-gulp.task("lint", function () {
-  return gulp.src(["*.coffee", "test/*.coffee"])
-    .pipe(coffeelint())
-    .pipe(coffeelint.reporter());
-});
-    
-gulp.task("compile", function () {
-  gulp.src("backbone.wamp.coffee")
-    .pipe(sourcemaps.init())
-    .pipe(coffee())
-    .pipe(sourcemaps.write({
-      includeContent: false,
-      sourceRoot: "./"
-    }))
-    .pipe(gulp.dest("./"));
-
-  return gulp.src("test/*.coffee")
-    .pipe(sourcemaps.init())
-    .pipe(coffee())
-    .pipe(sourcemaps.write({
-      includeContent: false,
-      sourceRoot: "../../test"
-    }))
-    .pipe(gulp.dest("tmp/test/"));
-});
-
-
-/* ********
-#    DEV
-# ****** */
-gulp.task("dev", function () {
-  gulp.watch(["*.coffee", "test/*.coffee"], ["build"]);
+gulp.task("lint", function (cb) {
+  cb();
 });
